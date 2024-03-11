fbf5083d3bb125255dfedcb34fd2e58b53f2de63
Kavin Gnanapandithan <kavin.balag@gmail.com>
2024-03-11 14:13:47 -0400

    Switch to UART polling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![deny(unsafe_code)]
 #![deny(warnings)]
+#![allow(unused_assignments)]
 #![no_main]
 #![no_std]
 #![feature(type_alias_impl_trait)]
@@ -15,10 +16,10 @@ mod app {
     use stm32f4xx_hal::{self as hal, pac::TIM5};
 
     use hal::{
-        dma::{config::DmaConfig, DmaFlag, PeripheralToMemory, Stream2, StreamsTuple, Transfer}, 
         i2c::{I2c1, Mode}, 
-        pac::{DMA2, TIM4, USART1}, 
-        prelude::*, rcc::RccExt, serial, 
+        pac::{TIM4, USART1}, 
+        prelude::*, rcc::RccExt, 
+        serial::{Config, Serial, Rx}, 
         timer::{self, Event}
     };
 
@@ -29,27 +30,14 @@ mod app {
     use crate::kalman::KalmanFilter;
     use lsm6dsox_driver::Lsm6dsox;
 
-
-    const BUFFER_SIZE: usize = 100;
-
-    type RxTransfer = Transfer<
-        Stream2<DMA2>,
-        4,
-        serial::Rx<USART1>,
-        PeripheralToMemory,
-        &'static mut [u8; BUFFER_SIZE],
-    >;
-
     #[shared]
     struct Shared {
-        #[lock_free]
-        rx_transfer: RxTransfer,
     }
 
     #[local]
     struct Local {
         /** FlySky Buffer **/
-        rx_buffer: Option<&'static mut [u8; BUFFER_SIZE]>,
+        rx: Rx<USART1, u8>,
         prod_flysky: Producer<'static, [u16; 16], 2>,
         con_flysky: Consumer<'static, [u16; 16], 2>,
 
@@ -99,44 +87,12 @@ mod app {
 
 
         let gpioa = dp.GPIOA.split();
+        let rx_pin = gpioa.pa10; // RX
 
-        // Initialize UART with DMA events
-        let rx_pin = gpioa.pa10;
-        let mut rx = dp
-            .USART1
-            .rx(
-                rx_pin,
-                serial::Config::default()
-                    .baudrate(115200.bps())
-                    .dma(serial::config::DmaConfig::Rx),
-                &clocks,
-            )
-            .unwrap();
+        // Configure USART1 with a baud rate of 115200
+        let rx: Rx<USART1, u8> =
+            Serial::rx(dp.USART1, rx_pin, Config::default().baudrate(115200.bps()), &clocks).unwrap();
 
-        // Listen UART IDLE event, which will be call USART1 interrupt
-        rx.listen_idle();
-
-        let dma2 = StreamsTuple::new(dp.DMA2);
-
-        // Note! It is better to use memory pools, such as heapless::pool::Pool. But it not work with embedded_dma yet.
-        // See CHANGELOG of unreleased main branch and issue https://github.com/japaric/heapless/pull/362 for details.
-        let rx_buffer1 = cortex_m::singleton!(: [u8; BUFFER_SIZE] = [0; BUFFER_SIZE]).unwrap();
-        let rx_buffer2 = cortex_m::singleton!(: [u8; BUFFER_SIZE] = [0; BUFFER_SIZE]).unwrap();
-
-        // Initialize and start DMA stream
-        let mut rx_transfer = Transfer::init_peripheral_to_memory(
-            dma2.2,
-            rx,
-            rx_buffer1,
-            None,
-            DmaConfig::default()
-                .memory_increment(true)
-                .fifo_enable(true)
-                .fifo_error_interrupt(true)
-                .transfer_complete_interrupt(true),
-        );
-
-        rx_transfer.start(|_rx| {});
 
         let gpiob = dp.GPIOB.split();
 
@@ -176,10 +132,12 @@ mod app {
         fc_timer.start(2000.millis()).unwrap();
         fc_timer.listen(Event::Update);
 
+        process_received_bytes::spawn().ok();
+
         (
-            Shared { rx_transfer },
+            Shared {  },
             Local {
-                rx_buffer: Some(rx_buffer2),
+                rx,
                 prod_flysky,
                 con_flysky,
 
@@ -197,94 +155,82 @@ mod app {
         )
     }
 
-    // Important! USART1 and DMA2_STREAM2 should the same interrupt priority!
-    #[task(binds = USART1, priority=1, local = [rx_buffer],shared = [rx_transfer])]
-    fn usart1(mut cx: usart1::Context) {
-        rprintln!("usart1 interrupt");
+    #[task(priority = 1, local=[prod_flysky, rx])]
+    async fn process_received_bytes(ctx: process_received_bytes::Context) {
+
+        let rx = ctx.local.rx;
+
+
+        loop {
+            rprintln!("process received bytes");
+            
+
+            let mut bytes = [0u8; 32];
+            let mut index = 0;
+
+
+            while index != 32 {
+                if let Ok(byte) = rx.read() {
+                    if index == 0 && byte != 32 {
+                        // Ignore bytes until the first byte is 32 (space character)
+                        continue;
+                    }
         
-        let transfer = &mut cx.shared.rx_transfer;
-
-        if transfer.is_idle() {
-            //rprintln!("transfer is idle");
-            // Calc received bytes count
-            let bytes_count = BUFFER_SIZE - transfer.number_of_transfers() as usize;
-
-            // Allocate new buffer
-            let new_buffer = cx.local.rx_buffer.take().unwrap();
-
-            // Replace buffer and restart DMA stream
-            let (buffer, _) = transfer.next_transfer(new_buffer).unwrap();
-
-            //let mut copied_buffer = [0; BUFFER_SIZE];
-
-            //copied_buffer.copy_from_slice(&buffer[..bytes_count]);
-
-            // Get slice for received bytes
-            //let _bytes = &buffer[..bytes_count];
-            //rprintln!("Data PRE-PROC: {:?}", _bytes);
-
-            //rprintln!("Byte count: {:?}", bytes_count);
-
-            if bytes_count == 32 {
-                process_received_bytes::spawn(*buffer).unwrap();
-            } else {
-                rprintln!("UART Data not 32 Bytes");
+                    bytes[index] = byte;
+                    index += 1;
+        
+                    // If the 32nd byte is encountered, process the buffer
+                    if index == 32 {
+                        //rprintln!("Data: {:?}", bytes);
+                        //index = 0; // Reset index for the next packet
+                    }
+                }
+            
             }
 
-            // Free buffer
+            //rprintln!("Bytes: {:?}", bytes);
 
-            *cx.local.rx_buffer = Some(buffer);
+            let mut channel_values: [u16; 16] = [0; 16];
 
+            for i in (0..bytes.len()).step_by(2) {
+                if i + 1 < bytes.len() {
+                    // Extract two bytes from the pair
+                    let byte1 = bytes[i];
+                    let byte2 = bytes[i + 1];
+
+                    // Combine the bytes by multiplying the second number by 256
+                    //let combined_value = u16::from(byte1) + u16::from(byte2) * 256;
+                    let combined_value = u16::from(byte1) | (u16::from(byte2) << 8);
+
+                    let channel_index = i / 2;
+                    //rprintln!("index: {:?}", channel_index);
+                    // Do something with the combined value (e.g., print or use it)
+
+                    channel_values[channel_index] = combined_value;
+                    //rprintln!("Combined Value: {}", combined_value);
+                } else {
+                    break
+                }
+            }
+
+            //rprintln!("Channels: {:?}", channel_values);
+            match ctx.local.prod_flysky.enqueue(channel_values) {
+                Ok(()) => {
+                    //rprintln!("Data sent");
+                }
+
+                Err(_err) => {
+                    // Other errors occurred, handle them appropriately
+                    // Example: println!("Error occurred while enqueueing data: {:?}", err);
+                    rprintln!("Flysky failed to enqueue");
+                }
+            }
+            //let _throttle = map(channel_values[5], 1000, 2000, 0, 100);
+            //let _desired_roll = map(channel_values[2], 1000, 2000, 0, 100);
+            //let _desired_pitch = map(channel_values[3], 1000, 2000, 0, 100);
+
+            Systick::delay(100.millis()).await;
         }
-    }
-
-
-    #[task(priority = 1, local=[prod_flysky])]
-    async fn process_received_bytes(ctx: process_received_bytes::Context, buffer: [u8; BUFFER_SIZE]) {
-        rprintln!("process received bytes");
-        //Systick::delay(3.millis()).await;
-        let bytes = &buffer[..32];
-
-        //rprintln!("Bytes: {:?}", bytes);
-
-        let mut channel_values: [u16; 16] = [0; 16];
-
-        for i in (0..bytes.len()).step_by(2) {
-            if i + 1 < bytes.len() {
-                // Extract two bytes from the pair
-                let byte1 = bytes[i];
-                let byte2 = bytes[i + 1];
-
-                // Combine the bytes by multiplying the second number by 256
-                //let combined_value = u16::from(byte1) + u16::from(byte2) * 256;
-                let combined_value = u16::from(byte1) | (u16::from(byte2) << 8);
-
-                let channel_index = i / 2;
-                //rprintln!("index: {:?}", channel_index);
-                // Do something with the combined value (e.g., print or use it)
-
-                channel_values[channel_index] = combined_value;
-                //rprintln!("Combined Value: {}", combined_value);
-            } else {
-                break
-            }
-        }
-
-        //rprintln!("Channels: {:?}", channel_values);
-        match ctx.local.prod_flysky.enqueue(channel_values) {
-            Ok(()) => {
-                //rprintln!("Data sent");
-            }
-
-            Err(_err) => {
-                // Other errors occurred, handle them appropriately
-                // Example: println!("Error occurred while enqueueing data: {:?}", err);
-                rprintln!("Flysky failed to enqueue");
-            }
-        }
-        //let _throttle = map(channel_values[5], 1000, 2000, 0, 100);
-        //let _desired_roll = map(channel_values[2], 1000, 2000, 0, 100);
-        //let _desired_pitch = map(channel_values[3], 1000, 2000, 0, 100);
         
     }
 
@@ -295,30 +241,15 @@ mod app {
     }
     */
 
-    #[task(binds = DMA2_STREAM2, priority=1, shared = [rx_transfer])]
-    fn dma2_stream2(mut cx: dma2_stream2::Context) {
-        let transfer = &mut cx.shared.rx_transfer;
-
-        let flags = transfer.flags();
-        transfer.clear_flags(DmaFlag::FifoError | DmaFlag::TransferComplete);
-        if flags.is_transfer_complete() {
-            // Buffer is full, but no IDLE received!
-            // You can process this data or discard data (ignore transfer complete interrupt and wait IDLE).
-
-            // Note! If you want process this data, it is recommended to use double buffering.
-            // See Transfer::init_peripheral_to_memory for details.
-        }
-    }
-    
 
     #[task(binds = TIM4, local=[kalman_timer, imu, i2c, x_kalman, y_kalman, prod_kalman], priority = 1)]
     fn sensor_fusion(ctx: sensor_fusion::Context) {
         ctx.local.kalman_timer.clear_all_flags();
-        ctx.local.kalman_timer.start(100.millis()).unwrap();
+        ctx.local.kalman_timer.start(15.millis()).unwrap();
 
         rprintln!("Sensor fusion");
 
-        let delta_sec = 0.1;
+        let delta_sec = 0.15;
 
         let accel_data: [f32; 3];
         let gyro_data: [f32; 3];
@@ -353,7 +284,7 @@ mod app {
     #[task(binds = TIM5, local = [fc_timer, con_kalman, con_flysky], priority = 1)]
     fn flight_controller(ctx: flight_controller::Context) {
         ctx.local.fc_timer.clear_all_flags();
-        ctx.local.fc_timer.start(1000.millis()).unwrap();
+        ctx.local.fc_timer.start(500.millis()).unwrap();
 
         rprintln!("FC");
 
